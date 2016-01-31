0502c4625e8800f004652f193fa66a8ac240a1f9
Alex Windett <windettalex@gmail.com>
2016-01-31 19:46:33 +0000

    refactoring js

--- js/app.js
+++ js/app.js
@@ -3,60 +3,8 @@ jQuery(document).ready(function($){
 
 	// Please annotate your JS so we can see what you’re thinking
 
-	// set variables for page elemets
-	var facesWrapper = $('.faces');
-
-	// Get the width of the image wrapper and find a third
-	// of this for the breakpoints
-	var sectionWidth = facesWrapper.width();
-	var sectionThird = sectionWidth / 3;
-
-	var firstThirdBoundary 	= sectionThird
-	var lastThirdBoundary 	= sectionWidth - sectionThird;
-	console.log('face width ' + firstThirdBoundary)
-
-	// Start off with the face on image by hiding the first two
-	var defaultimages = {
-
-		init: function() {
-			$('.faces').find('.face-1').hide();
-			$('.faces').find('.face-2').show();
-			$('.faces').find('.face-3').hide();
-		}
-	}
-
 	defaultimages.init();
 
-	$('.faces').mousemove(function (e){
-		// find the exposition of mouse inside the wrapper
-	    var elm = $(this);
-	    var xPos = e.pageX - elm.offset().left;
-		var faceStyle = $(this).attr('id');
-		console.log(faceStyle);
-	    console.log(xPos);
-
-		if ( xPos <= firstThirdBoundary ) {
-			// if in first third show left angled face
-			console.log('this is the first third');
-			$('.faces#' + faceStyle).find('.face-1').show();
-			$('.faces#' + faceStyle).find('.face-2').hide();
-		} else if ( xPos >= lastThirdBoundary ) {
-			// if in first third show front forward face
-			console.log('this is the middle third');
-			$('.faces#' + faceStyle).find('.face-3').show();
-			$('.faces#' + faceStyle).find('.face-2').hide();
-		} else {
-			// if in first third show right angled face
-			console.log('this is the last third');
-			$('.faces#' + faceStyle).find('.face-2').show();
-			$('.faces#' + faceStyle).find('.face-1').hide();
-			$('.faces#' + faceStyle).find('.face-3').hide();
-		}
-	});
-
-	// When the mouse leaves the area, return images to page loaded state
-	$('.faces').mouseleave(function (e) {
-		defaultimages.init();
-	});
+	defaultimages.rollOver();  
 
 });
